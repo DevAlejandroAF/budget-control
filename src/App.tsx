53415c22541352effbea64f54c208db03f619f00
DevAlejandroAF <alejandroavi95@hotmail.com>
2024-09-10 15:27:14 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import BudgetForm from "./components/BudgetForm";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <BudgetForm />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/BudgetForm.tsx
@@ -0,0 +1,3 @@
+export default function BudgetForm() {
+  return <h2>BudgetForm</h2>;
+}
